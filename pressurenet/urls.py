822d26e6ac0f739a3c25588b97362867df0c7ad2
Jared Kerim <pwbdecker@gmail.com>
2013-03-30 16:57:18 -0400

    Added updated readings path in urls

--- pressurenet/urls.py
+++ pressurenet/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^api-auth/', include('djangorestframework.urls', namespace='djangorestframework')),
-    url(r'^', include('readings.urls')),
+    url(r'^', include('pressurenet.readings.urls')),
 )
