6f2696a68a2a42bbcf0214bc2bb1a84c4bb4c088
Jared Kerim <pwbdecker@gmail.com>
2013-04-23 01:46:50 -0400

    Added readings per hour to readings admin

--- readings/admin.py
+++ readings/admin.py
@@ -1,9 +1,26 @@
+import datetime
+import time
+
 from django.contrib import admin
 from readings.models import Reading, Customer, CustomerCallLog
 
 
 class ReadingAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'latitude', 'longitude', 'reading', 'daterecorded')
+
+    def changelist_view(self, request, extra_context=None):
+        hour_ago = datetime.datetime.now() - datetime.timedelta(hours=1)
+        hour_ago = time.mktime(hour_ago.timetuple())
+        readings = Reading.objects.filter(daterecorded__gte=(hour_ago*1000)).count()
+        context = {
+            'readings_per_hour': readings,
+        }
+
+        if extra_context:
+            context.update(extra_context)
+
+        return super(ReadingAdmin, self).changelist_view(request, context)
+        
 
 admin.site.register(Reading, ReadingAdmin)
 
