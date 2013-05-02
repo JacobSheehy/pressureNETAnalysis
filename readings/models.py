309173a343fb24584e703f757fbcbed111ceeb0d
Jared Kerim <pwbdecker@gmail.com>
2013-05-02 02:04:00 -0400

    Added new fields to readings for new api

--- readings/models.py
+++ readings/models.py
@@ -3,17 +3,21 @@ from django.db import models
 
 class Reading(models.Model):
     """Barometer reading from pressureNET"""
-    user_id = models.CharField(max_length=255, db_index=True)
     date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    user_id = models.CharField(max_length=255, db_index=True)
     latitude = models.FloatField(db_index=True)
     longitude = models.FloatField(db_index=True)
+    altitude = models.FloatField(default=0.0)
     reading = models.FloatField()
+    reading_accuracy = models.FloatField()
+    provider = models.CharField(max_length=255, default='')
+    observation_type = models.CharField(max_length=255, default='')
+    observation_unit = models.CharField(max_length=255, default='')
+    sharing = models.CharField(max_length=255)
     daterecorded = models.BigIntegerField(db_index=True)
     tzoffset = models.BigIntegerField()
-    sharing = models.CharField(max_length=255)
     client_key = models.CharField(max_length=255)
     location_accuracy = models.FloatField()
-    reading_accuracy = models.FloatField()
 
     class Meta:
         verbose_name = 'reading'
